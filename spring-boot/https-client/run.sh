86be3800fb318f3ce873dd893017f5ff989d2f81
martinez <francisco.martinez@disy.net>
2024-09-18 11:26:24 +0200

    remove test containers

--- spring-boot/https-client/run.sh
+++ spring-boot/https-client/run.sh
@@ -1,20 +1,20 @@
 URL="${1:-https://untrusted-root.badssl.com}"
 
 # Test 1: SSL Bundle with environment variables
-docker run \
+docker run --rm \
     -e SPRING_SSL_BUNDLE_JKS_MYBUNDLE_TRUSTSTORE_LOCATION=/app/customTrustStore.jks \
     -e SPRING_SSL_BUNDLE_JKS_MYBUNDLE_TRUSTSTORE_PASSWORD=changeit \
     https-client "$URL"
 
 # Test 2: SSL Bundle with properties
-docker run \
+docker run --rm \
     -e JAVA_OPTS="-Dspring.ssl.bundle.jks.myBundle.truststore.location=/app/customTrustStore.jks -Dspring.ssl.bundle.jks.myBundle.truststore.password=changeit" \
     https-client "$URL"
 
 # Test 3: Standard javax.net.ssl properties
-docker run \
+docker run --rm \
     -e JAVA_OPTS="-Djavax.net.ssl.trustStore=/app/customTrustStore.jks -Djavax.net.ssl.trustStorePassword=changeit" \
     https-client "$URL"
 
 # Test 4: Default Java CA certificates
-docker run https-client https://example.com
+docker run --rm https-client https://example.com
